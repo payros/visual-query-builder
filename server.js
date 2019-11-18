a606291dd6f1954228b9cc4cd12473fad8ed4c4f
Pedro Carvalho <payros@gmail.com>
2019-11-18 18:48:43 -0500

    Fix queries with "LIMIT"

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ if(process.argv.length > 2 && process.argv[2] === 'prod') {
 	const middleware = require('webpack-dev-middleware');
 	const configs = require('./webpack.config.js')
 	const compiler = webpack(configs);
-	app.use(middleware(compiler, { index:'./public/index.html' }));	
+	app.use(middleware(compiler, { index:'./public/index.html' }));
 }
 
 app.use(express.static('public'))
 
 app.get('/query', (req, res) => {
 	const terminatorIdx = req.query.q.indexOf(';');
-	const constructedQuery = (terminatorIdx > -1 ? req.query.q.substring(0, terminatorIdx) : req.query.q) + " LIMIT 1000"
+	const constructedQuery = (terminatorIdx > -1 ? req.query.q.substring(0, terminatorIdx) : req.query.q).replace(/\sLIMIT.*$/i, '') + " LIMIT 1000"
 	console.log(constructedQuery)
 	//Restrict to read queries - A bit naive, but it will do for now
 	if(constructedQuery.substring(0,6).toUpperCase() === "SELECT"){
 		pool.query(constructedQuery).then(rs => {
 			res.json(rs.rows)
 		}).catch(e => {
-			res.status(500).send(e.message)
+			res.status(500).send(e.message.replace(' at or near "LIMIT"', ''))
 		})
 	} else {
 		res.status(500).send("This query is not supported")
